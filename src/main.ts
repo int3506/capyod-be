9bdbce65c0de7d92142ea7fc7066fb2b2b2203d9
Nguyễn Minh Hiển <65478890+nmhienbn@users.noreply.github.com>
2024-12-02 14:55:06 +0700

    allow fe's access (#9)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function boostrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Hỗ trợ cookie
+  });
+
   const config = new DocumentBuilder()
     .setTitle('CapyOD')
     .setDescription('The CapyOD API description')
